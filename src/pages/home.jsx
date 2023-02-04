2e9ef414af1e3f8c25fb31495d30efbd1d06d0f0
Rahul <rahulkumarsinha5316@gmail.com>
2023-02-04 12:26:00 -0800

    Initial Commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+import {Loader, Card, FormField} from "../components"
+const home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  return (
+    <section className = "max-w-7xl mx-auto">
+      <div>
+        <h1 className></h1>
+      </div>
+    </section>
+
+  )
+}
+
+export default home
